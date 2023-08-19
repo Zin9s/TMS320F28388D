ddec6cde75843a9a8bbfbbc6f7312fab285f7797
Jinsu Park <jinsupark2323@gmail.com>
2023-08-19 21:03:20 +0900

    SYS/BIOS Step2_2023.08.18

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # TMS320F28388D
 
-# SYS/BIOS Step1_2023.08.18
+# SYS/BIOS Step2_2023.08.18
 # Project : TMS320F28388D Study
 # Editor : Zin9s
-# Web : https://zin9.tistory.com/41
+# Web : https://zin9.tistory.com/42
 
-1. create SYS/BIOS Project
+1. create 2 tasks
+2. LED toggle
+3. Added Library folder from C2000ware;common, driverlib, headers
--- main_CPU1.c
+++ main_CPU1.c
@@ -11,36 +11,48 @@
 
 #include <ti/sysbios/knl/Task.h>
 
+#include <device.h>
+#include <f2838x_examples.h>
+#include <f2838x_device.h>
+
 /*
  *  ======== taskFxn ========
  */
-Void taskFxn(UArg a0, UArg a1)
+void TaskA(void)
 {
-    System_printf("enter taskFxn()\n");
-
-    Task_sleep(10);
-
-    System_printf("exit taskFxn()\n");
-
-    System_flush(); /* force SysMin output to console */
+    while(1)
+    {
+        GPIO_togglePin(DEVICE_GPIO_PIN_LED1);
+        Task_sleep(200); // [ms]
+    }
 }
 
+void TaskB(void)
+{
+    while(1)
+    {
+        GPIO_togglePin(DEVICE_GPIO_PIN_LED2);
+        Task_sleep(500); // [ms]
+    }
+}
 /*
  *  ======== main ========
  */
 Int main()
 { 
-    Task_Handle task;
-    Error_Block eb;
+    //
+    // Initialize device clock and peripherals
+    //
+    Device_init();
 
-    System_printf("enter main()\n");
-
-    Error_init(&eb);
-    task = Task_create(taskFxn, NULL, &eb);
-    if (task == NULL) {
-        System_printf("Task_create() failed!\n");
-        BIOS_exit(0);
-    }
+    //
+    // Initialize GPIO and configure the GPIO pin as a push-pull output
+    //
+    Device_initGPIO();
+    GPIO_setPadConfig(DEVICE_GPIO_PIN_LED1, GPIO_PIN_TYPE_STD);
+    GPIO_setDirectionMode(DEVICE_GPIO_PIN_LED1, GPIO_DIR_MODE_OUT);
+    GPIO_setPadConfig(DEVICE_GPIO_PIN_LED2, GPIO_PIN_TYPE_STD);
+    GPIO_setDirectionMode(DEVICE_GPIO_PIN_LED2, GPIO_DIR_MODE_OUT);
 
     BIOS_start();    /* does not return */
     return(0);
