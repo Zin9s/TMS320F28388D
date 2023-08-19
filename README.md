bdb14b2b39ca0a26a593b489053eba717f42be36
Jinsu Park <jinsupark2323@gmail.com>
2023-08-20 00:57:46 +0900

    SYS/BIOS Step5_2023.08.19

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # TMS320F28388D
 
-# SYS/BIOS Step4_2023.08.19
+# SYS/BIOS Step5_2023.08.19
 # Project : TMS320F28388D Study
 # Editor : Zin9s
-# Web : https://zin9.tistory.com/44
+# Web : https://zin9.tistory.com/45
 
-1. create Timer
-2. swi is called by timer
-3. LED toggle
+1. compare TimerC(task timer) with TimerC(bios timer)
+2. create hwi with EPWM
+3. swi is called by TimerC
+4. hwi toggles LED
--- main_CPU1.c
+++ main_CPU1.c
@@ -11,6 +11,7 @@
 
 #include <ti/sysbios/knl/Task.h>
 #include <ti/sysbios/knl/Swi.h>
+#include <ti/sysbios/hal/Hwi.h>
 
 #include <device.h>
 #include <f2838x_examples.h>
@@ -19,18 +20,24 @@
 //
 // Globals
 //
+uint32_t TaskA_100mscount = 0;
+uint32_t TaskB_10mscount = 0;
 uint32_t Swi0IntCount = 0;
 uint32_t TimerCIntCount = 0;
 uint16_t TimerC1msCount = 0;
-uint16_t LEDcount = 0;
+uint32_t TimerDIntCount = 0;
+uint32_t TimerD1msCount = 0;
+uint32_t EPwm1IntCount = 0;
+uint32_t LED1count = 0;
+uint32_t LED2count = 0;
+int32_t diffCount = 0;
 
 //
 // Swi Handler
 //
 extern ti_sysbios_knl_Swi_Handle swi0;
 
-
-void Swi0_ISR(UArg arg0, UArg arg1);
+void init_EPwm1(void);
 
 
 /*
@@ -40,8 +47,8 @@ void TaskA(void)
 {
     while(1)
     {
-        GPIO_togglePin(DEVICE_GPIO_PIN_LED1);
-        Task_sleep(200); // [ms]
+        TaskA_100mscount++;
+        Task_sleep(100); // [ms]
     }
 }
 
@@ -49,7 +56,7 @@ void TaskB(void)
 {
     while(1)
     {
-//        Swi_post(swi0);
+        TaskB_10mscount++;
         Task_sleep(10); // [ms]
     }
 }
@@ -57,6 +64,9 @@ void TaskB(void)
 /*
  *  ======== timerFxn ========
  */
+// TimerC() -> task timer0
+// TimerD() -> bios timer1
+// TimerC() and TimerD() is running every 31.25us.
 void TimerC(void)
 {
     Swi_post(swi0);
@@ -67,11 +77,103 @@ void TimerC(void)
     }
 }
 
+void TimerD(void)
+{
+    TimerDIntCount++;
+    if ((TimerDIntCount % 32) == 0)
+    {
+        TimerD1msCount++;
+    }
+    diffCount = Swi0IntCount - EPwm1IntCount;
+}
+
+/*
+ *  ======== swiFxn ========
+ */
+void Swi0_ISR(UArg arg0, UArg arg1)
+{
+    Swi0IntCount++;
+    LED2count++;
+
+    //
+    // Toggle LED2 when the count reaches 16000
+    //
+    if (LED2count == 16000)
+    {
+        GPIO_togglePin(DEVICE_GPIO_PIN_LED2);
+        LED2count=0;
+    }
+}
+
+/*
+ *  ======== hwiFxn ========
+ */
+void EPwm1_ISR(UArg arg0)
+{
+    EPwm1IntCount++;
+    LED1count++;
+    //
+    // Toggle LED1 every 500ms
+    //
+    if (LED1count == 16000)
+    {
+        GPIO_togglePin(DEVICE_GPIO_PIN_LED1);
+        LED1count=0;
+    }
+
+    //
+    // Clear INT flag for this timer
+    //
+    EPwm1Regs.ETCLR.bit.INT = 1;    // Event Trigger Clear Register
+}
+
+/*
+ *  ======== initFxn ========
+ */
+#define TBCLK 200000000
+#define PWMFREQ 32000
+uint32_t EPWM1_TIMER_TBPRD = 0;
+
+void init_EPwm1(void)
+{
+    EALLOW;     // It is needed to write to EALLOW protected registers.
+
+    //
+    // Setup TBCLK
+    //
+    EPWM1_TIMER_TBPRD = TBCLK/PWMFREQ/2;        // 3125 = 200MHz/32kHz/2
+    EPwm1Regs.TBPRD = EPWM1_TIMER_TBPRD;       // Set timer period 3125 TBCLKs
+    EPwm1Regs.TBPHS.bit.TBPHS = 0x0000;        // Phase is 0
+    EPwm1Regs.TBCTR = 0x0000;                  // Clear counter
+
+    //
+    // Setup counter mode
+    //
+    EPwm1Regs.TBCTL.bit.CTRMODE = TB_COUNT_UPDOWN; // Count up and down
+    EPwm1Regs.TBCTL.bit.PHSEN = TB_DISABLE;        // Disable phase loading
+
+    //
+    // Configure EPWM to run at SYSCLK
+    //
+    ClkCfgRegs.PERCLKDIVSEL.bit.EPWMCLKDIV = SYSCTL_EPWMCLK_DIV_1;    // 200MHz
+    EPwm1Regs.TBCTL.bit.HSPCLKDIV = TB_DIV1;       // Clock ratio to SYSCLKOUT
+    EPwm1Regs.TBCTL.bit.CLKDIV = TB_DIV1;
+
+    //
+    // Interrupt where we will change the Compare Values
+    //
+    EPwm1Regs.ETSEL.bit.INTSEL = ET_CTR_ZERO;     // Select INT on Zero event
+    EPwm1Regs.ETSEL.bit.INTEN = 1;                // Enable INT
+    EPwm1Regs.ETPS.bit.INTPRD = ET_1ST;           // Generate INT on 1 event
+
+    EDIS;       // It is needed to disable write to EALLOW protected registers.
+}
+
 /*
  *  ======== main ========
  */
 Int main()
-{ 
+{
     //
     // Initialize device clock and peripherals
     //
@@ -86,22 +188,11 @@ Int main()
     GPIO_setPadConfig(DEVICE_GPIO_PIN_LED2, GPIO_PIN_TYPE_STD);
     GPIO_setDirectionMode(DEVICE_GPIO_PIN_LED2, GPIO_DIR_MODE_OUT);
 
+    //
+    // Initialize EPwm1
+    //
+    init_EPwm1();
+
     BIOS_start();    /* does not return */
     return(0);
-}
-
-void Swi0_ISR(UArg arg0, UArg arg1)
-{
-    Swi0IntCount++;
-    LEDcount++;
-
-    //
-    // Toggle LED2 when the count reaches 32000
-    //
-    if (LEDcount == 32000)
-    {
-        GPIO_togglePin(DEVICE_GPIO_PIN_LED2);
-        LEDcount=0;
-    }
-
 }
